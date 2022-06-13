99286fd7331b26e0d3eb208093dab53dcd902e63
ev9en1y <rivnosc@gmail.com>
2022-06-13 22:10:41 +0300

    add class Shape

---
+++ src/Shape.java
@@ -0,0 +1,14 @@
+abstract public class Shape {
+    private final String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+
+
